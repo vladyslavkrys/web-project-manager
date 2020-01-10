6a57c2fa59f59c27b2ce2fca669e27ff8cec6376
Vladyslav Krys <vladyslav_krys@epam.com>
2020-01-10 20:50:52 +0200

    add security processing (front-end side)

---
+++ manager-react-client/src/reducers/securityReducer.js
@@ -0,0 +1,24 @@
+import { SET_CURRENT_USER } from "../actions/types";
+
+const initialState = {
+  validToken: false,
+  user: {}
+};
+
+const booleanActionPayload = payload => {
+  return payload ? true : false;
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        validToken: booleanActionPayload(action.payload),
+        user: action.payload
+      };
+
+    default:
+      return state;
+  }
+}
